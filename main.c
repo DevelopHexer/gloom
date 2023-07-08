e159a32922b40f5951b7cc8cb0007a22fe4d09ea
DevelopHexer <138876992+DevelopHexer@users.noreply.github.com>
2023-07-08 02:32:30 +0200

    added contract

--- main.c
+++ main.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
+#include "project.c"
 int main(){
     int x=0;
     for(int i=0;i<5;i++){
         x=x+1;
     }
-    int y = x-1;
+    int y = x-7;
+    devuelvenum(y);
+    int a;
+    while(x!=0){break;}
     return 0;
 }
